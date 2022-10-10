84d852c7357615f6b4d795d956fe182a86fd0ee7
Eliseu Bueno <sturarobuenoeliseu@gmail.com>
2022-10-10 00:08:19 -0300

    Preparação de componentes que serão usados no projeto (forms, buttons, imgs, carousel, navbar, footer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <NavBar/>    
+    <NavBar :logo="logo_src" :alt="app_name"/> 
     <router-view/>
     <Footer/>
 </template>
@@ -10,7 +10,14 @@ import Footer from './components/layouts/Footer.vue';
 export default {
     name: "App",
     components: {
-        NavBar, Footer
+    NavBar,
+    Footer
+},
+    data() {
+        return {
+            logo_src: '/img/logo.png',
+            app_name: 'Make Your Burger',
+        }
     }
 }
 </script>
---
+++ src/components/forms/FormBurger.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <h3>Formulário de Cadastro de Burguers</h3>
+        <form action="">
+            <div class="form-floating mb-3">
+                <input type="email" class="form-control" id="floatingInput" placeholder="Digite o nome do cliente">
+                <label for="floatingInput">Nome do cliente:</label>
+            </div>
+            <div>
+                <label for="pao">Escolha o pão</label>
+                <select class="form-select form-select-lg mb-3" name="pao" id="pao" v-model="pao">
+                    <option selected>Selecione o pão</option>
+                    <option value="integral">Integral</option>
+                </select>
+            </div>
+            <div>
+                <label for="carne">Escolha a carne</label>
+                <select class="form-select form-select-lg mb-3" name="carne" id="carne" v-model="carne">
+                    <option selected>Selecione a carne</option>
+                    <option value="contra-file">Contra-Filé</option>
+                </select>
+            </div>
+            <div class="form-check form-switch text-start">
+                <label class="form-check-label" for="opcionais">Selecione os opcionais:</label>
+                <div>
+                    <input 
+                        class="form-check-input" 
+                        type="checkbox" 
+                        role="switch" 
+                        id="opcionais" 
+                        name="opcionais" 
+                        v-model="opcionais" 
+                        value="opcionais"
+                    >
+                    <span>Salame</span>
+                </div>
+            </div>
+            <div>
+                <button type="submit" class="btn btn-primary" value="Criar meu Burguer">Criar</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FormBurger',
+}
+</script>
